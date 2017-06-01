7a34e1d641948e0bf4e8a0d815dfd5b724e8ba03
ad5915 <dan.alex97@yahoo.com>
2017-06-01 18:23:01 +0100

    Just playing along with javascript and making a basic navigation bar and listeners.

--- src/main/resources/static/app/index.controller.js
+++ src/main/resources/static/app/index.controller.js
@@ -6,7 +6,7 @@
 
     angular
         .module('app')
-        .controller('IndexController', IndexController);
+        .controller('IndexController', [IndexController]);
 
     IndexController.$inject = ['$http'];
 

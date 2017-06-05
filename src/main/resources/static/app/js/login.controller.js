4d41e10e416a17cefe546a64f22ac80f9dd24ed3
ad5915 <dan.alex97@yahoo.com>
2017-06-05 22:14:15 +0100

    Implemented basic login with the new frontend design. Also, the callbacks now are made differently and resources are exposed diffrently.

---
+++ src/main/resources/static/app/js/login.controller.js
@@ -0,0 +1,52 @@
+
+
+/**
+ * Created by ad5915 on 05/06/17.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', ['$scope', '$http', function($scope, $http) {
+            var vm = this;
+
+            vm.record  = record;
+            vm.startSession = startSession;
+
+            vm.user = "";
+            vm.password = "";
+
+            function record() {
+                vm.user = $scope.inputUser;
+                vm.password = $scope.passUser;
+
+                console.log(vm.user);
+                console.log(vm.password);
+
+                startSession();
+                document.forms[0].submit();
+            }
+
+            function startSession() {
+                var url = "/session/start";
+                var data = {
+                    "login" : vm.user,
+                    "password" : vm.password
+                };
+
+                console.log(vm.user);
+                console.log(vm.password);
+
+                $http.post(url, data).then(function () {
+                    console.log("Started new session for user " + vm.user);
+                });
+            }
+
+            init();
+
+            function init() {
+            }
+        }])
+})();
+

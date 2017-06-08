4904b880b43c5bd6f92edfeb86026048f0f46280
ad5915 <dan.alex97@yahoo.com>
2017-06-08 01:11:13 +0100

    Injected a repository into ranking manager. Now we should have ratings.

---
+++ src/main/java/group15/cerebro/session/models/Ranker.java
@@ -0,0 +1,43 @@
+package group15.cerebro.session.models;
+
+import group15.cerebro.MainApplication;
+import group15.cerebro.entities.Usr;
+import group15.cerebro.repositories.UserRepository;
+
+public class Ranker {
+    private Usr user;
+    private UserRepository repository;
+
+    private final int MAX_DIFF = 10;
+    private final int FACT = 3;
+    private final int SCALE = 10000;
+    private final int SEGM_SIZE = 1000;
+
+    private int segment;
+
+    public Ranker(Usr user, UserRepository repository) {
+        this.user = user;
+        this.segment = user.getRating() / SEGM_SIZE;
+        this.repository = repository;
+    }
+
+    public void update(boolean correct) {
+        int newRating = user.getRating();
+        int oldRating = newRating;
+        if (correct) {
+            int gain = FACT * (MAX_DIFF - segment);
+            newRating += gain;
+        } else {
+            int loss = FACT * segment / 2;
+            newRating -= loss;
+        }
+        newRating = Math.max(0, newRating);
+        newRating = Math.min(SCALE, newRating);
+        user.setRating(newRating);
+
+        repository.save(user);
+        MainApplication.logger.warn("Rating update: old(" + oldRating
+                +  "), new(" + newRating + ")");
+    }
+
+}
